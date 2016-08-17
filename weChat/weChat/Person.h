ba819ed7e73c3197e050a5a2fbb34798f23c7901
ray <ray@163.com>
2016-08-17 10:12:33 +0800

    创建了person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by ray威LL on 16/8/17.
+//  Copyright © 2016年 ray威LL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by ray威LL on 16/8/17.
+//  Copyright © 2016年 ray威LL. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
